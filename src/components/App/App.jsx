dd156bc1734382f3db48e15f7a865479ee6e145b
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2024-08-23 18:41:50 +0300

    added components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,39 +5,6 @@ import { Options } from "../Options/Options.jsx";
 import { Feedback } from "../Feedback/Feedback.jsx";
 // import styles from "./App.module.css";
 
-// export function Description() {
-//     return (
-//         <div className={styles.reviewsContainer}>
-//            <h1>Sip Happens Café</h1>
-//            <p>Please leave your feedback about our service by selecting one of the options below.</p>
-//         </div>   
-//     )
-// }
-// export function Options({ updateFeedback }) {
-//     return (
-//         <ul className={styles.reviewsList}>
-//             <li><button onClick={() => updateFeedback('Good')}>Good</button></li>
-//             <li><button onClick={() => updateFeedback('Neutral')}>Neutral</button></li>
-//             <li><button onClick={() => updateFeedback('Bad')}>Bad</button></li>
-//             <li><button onClick={() => updateFeedback('Reset')}>Reset</button></li>
-//         </ul>
-//     )
-// }
-// export function Feedback({ feedback }) {
-//     const totalFeedback = feedback.Good + feedback.Neutral + feedback.Bad;
-//     const positivePercentage = totalFeedback > 0
-//     ? Math.round((feedback.Good / totalFeedback) * 100) 
-//     : 0;
-//     return (
-//         <div className={styles.feedbacks}>
-//             <p>Good: {feedback.Good}</p>
-//             <p>Neutral: {feedback.Neutral}</p>
-//             <p>Bad: {feedback.Bad}</p>
-//             <p>Total: {totalFeedback}</p>
-//             <p>Positive: {positivePercentage}%</p>
-//         </div>
-//     );
-// }
 
 
 export default function App() {
@@ -45,14 +12,16 @@ export default function App() {
         const savedFeedback = localStorage.getItem('feedback');
         return savedFeedback ? JSON.parse(savedFeedback) : { Good: 0, Neutral: 0, Bad: 0 };
     });
+
     useEffect(() => {
         localStorage.setItem('feedback', JSON.stringify(feedback));
     }, [feedback]);
 
     const updateFeedback = feedbackType => {
+        const formattedType = feedbackType.charAt(0).toUpperCase() + feedbackType.slice(1);
         setFeedback (prevFeedback => ({
             ...prevFeedback,
-            [feedbackType.charAt(0).toUpperCase() + feedbackType.slice(1)]: prevFeedback[feedbackType.charAt(0).toUpperCase() + feedbackType.slice(1)] + 1
+            [formattedType]: prevFeedback[formattedType] + 1
         }));
     };
 
@@ -60,7 +29,7 @@ export default function App() {
         if (option === 'Reset') {
             setFeedback({Good: 0, Neutral: 0, Bad: 0});
         } else {
-            updateFeedback(option.toLowerCase());
+            updateFeedback(option);
         };
     };    
 
